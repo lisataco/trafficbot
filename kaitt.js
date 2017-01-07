d2a294b4548e21e0a0cd681fe8713214141d6848
lisataco <lisataco@gmail.com>
2017-01-07 12:45:51 -0500

    kaitt.js

--- kaitt.js
+++ kaitt.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var kaitt = express();
-var audio = new Audio("Knight_RIght.mp3");
+var audio = audio("Knight_Right.mp3");
 
 kaitt.post(function(req,res){
 window.open("https://goo.gl/maps/NAYL5n2TRz12")	;
