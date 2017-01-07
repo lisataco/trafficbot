ddc048d241828822f2f6806adfc8eea6966a1ce6
lisataco <lisataco@gmail.com>
2017-01-07 13:10:11 -0500

    kaitt

--- kaitt.js
+++ kaitt.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var kaitt = express();
-var audio = audio("Knight_Right.mp3");
+var express = require('express')
+var kaitt = express()
+var audio = new Audio("Knight_Right.mp3")
 
 kaitt.post(function(req,res){
-window.open("https://goo.gl/maps/NAYL5n2TRz12")	;
-audio.play();
+	window.open("https://goo.gl/maps/NAYL5n2TRz12")
+	audio.play()
 
-});
+})
 
 kaitt.listen(5000, function(){
 	console.log('Kaitt app listening on port 5000!');
-});
+})
