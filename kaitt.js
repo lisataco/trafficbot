77a5454ceb1d97f0f3619c4416b02f1f617d73b8
lisataco <lisataco@gmail.com>
2017-01-07 14:07:14 -0500

    kaitt

--- kaitt.js
+++ kaitt.js
@@ -1,13 +1,33 @@
-var express = require('express')
-var kaitt = express()
-var audio = new Audio("Knight_Right.mp3")
+process.env.DEBUG = 'actions-on-google:*';
+let Assistant = require('actions-on-google').ApiAiAssistant;
+let express = require('express');
+let bodyParser = require('body-parser');
 
-kaitt.post(function(req,res){
-	window.open("https://goo.gl/maps/NAYL5n2TRz12")
-	audio.play()
+let kaitt = express();
+kaitt.use(bodyParser.json({type: 'application/json'}));
 
-})
+let Audio = require('Audio');
+var audio = new Audio("Knight_Right.mp3");
 
-kaitt.listen(5000, function(){
-	console.log('Kaitt app listening on port 5000!');
-})
+kaitt.post('/',function(req,res){
+	const assistant = new Assistant({request: req, response: res});
+	console.log('Request headers: ' + JSON.stringify(req.headers));
+	console.log('Request body: ' + JSON.stringify(req.body));
+
+	function responseHandler (assistant) {
+		window.open("https://goo.gl/maps/NAYL5n2TRz12");
+		audio.play();
+    	assistant.tell('911 alerted. All lights are green. On our way to the hospital.');
+  	assistant.handleRequest(responseHandler);
+  }
+});
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  let server = kaitt.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('KaittApp listening on port %s', port);
+  });
+}
+module.exports = kaitt;
